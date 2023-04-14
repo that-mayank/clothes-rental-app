675ffaf003a99b5bd87c711fe433d99b27795a5e
Mayank <thatmayank@gmail.com>
2023-04-14 21:59:57 +0530

    Added switch profile

--- src/main/java/com/nineleaps/leaps/config/MessageStrings.java
+++ src/main/java/com/nineleaps/leaps/config/MessageStrings.java
@@ -5,6 +5,6 @@ public class MessageStrings {
     public static final String AUTH_TOKEN_NOT_PRESENT = "Authentication Token Not Present";
     public static final String AUTH_TOKEN_NOT_VALID = "Authentication Token Not Valid";
     public static final String USER_CREATED = "User Created Successfully";
-    public static final String ID_NOT_PRESENT = "primary key is required for updating";
+    public static final String ID_NOT_PRESENT = "Primary key is required for updating";
     public static final String WRONG_PASSWORD = "Please Check The Credentials";
 }

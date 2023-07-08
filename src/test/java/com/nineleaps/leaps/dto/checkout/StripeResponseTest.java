551ebeea2fbe73ec076d1f34a35ba88914f7841a
Mayank <thatmayank@gmail.com>
2023-07-08 16:11:56 +0530

    Added test cases for checkout dto with 100 percent code coverage.

--- src/test/java/com/nineleaps/leaps/dto/checkout/StripeResponseTest.java
+++ src/test/java/com/nineleaps/leaps/dto/checkout/StripeResponseTest.java
@@ -7,15 +7,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StripeResponseTest {
 
+    private StripeResponse stripeResponse;
+
     @BeforeEach
     void setUp() {
+        stripeResponse = new StripeResponse();
     }
 
     @Test
     void getSessionId() {
+        // Prepare
+        String sessionId = "example-session-id";
+        stripeResponse.setSessionId(sessionId);
+
+        // Execute
+        String result = stripeResponse.getSessionId();
+
+        // Verify
+        assertEquals(sessionId, result);
     }
 
     @Test
     void setSessionId() {
+        // Prepare
+        String sessionId = "example-session-id";
+
+        // Execute
+        stripeResponse.setSessionId(sessionId);
+
+        // Verify
+        assertEquals(sessionId, stripeResponse.getSessionId());
     }
 }

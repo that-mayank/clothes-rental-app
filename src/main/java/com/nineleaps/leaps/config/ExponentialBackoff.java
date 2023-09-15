4c5d4c339573af2bcabf50beeca6e718cc8c5263
yokesnineleaps <yokes.e@nineleaps.com>
2023-09-15 16:03:01 +0530

    Added ExponentialBackoff Strategy with Resilience4j Retry Mechanism for uploading images in Amazon S3

---
+++ src/main/java/com/nineleaps/leaps/config/ExponentialBackoff.java
@@ -0,0 +1,22 @@
+package com.nineleaps.leaps.config;
+
+import io.github.resilience4j.retry.RetryConfig;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
+public class ExponentialBackoff {
+    private ExponentialBackoff() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static RetryConfig buildRetryConfig(int maxAttempts, long waitDuration) {
+        return RetryConfig.custom()
+                .maxAttempts(maxAttempts)
+                .waitDuration(Duration.ofMillis(waitDuration))
+                .retryExceptions(S3Exception.class, IOException.class, TimeoutException.class)  // Add specific exceptions if needed
+                .build();
+    }
+}

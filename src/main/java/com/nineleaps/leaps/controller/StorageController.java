c0eaaa585d0b10ae3222af39a5d39a65fb8c40c8
Mayank <thatmayank@gmail.com>
2023-10-20 16:46:49 +0530

    Refactor: Storage Controller and Updated Test cases for same

--- src/main/java/com/nineleaps/leaps/controller/StorageController.java
+++ src/main/java/com/nineleaps/leaps/controller/StorageController.java
@@ -14,7 +14,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,53 +23,35 @@ import java.util.List;
 @Slf4j
 @AllArgsConstructor
 @Api(tags = "Storage Api")
-
 public class StorageController {
 
     //Linking layers using constructor injection
-
     private final StorageServiceInterface storageServiceInterface;
 
     // API : To upload images of the product to s3
-
     @ApiOperation(value = "API : To upload images of the product to s3")
     @PostMapping("/upload")
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasAuthority('OWNER')")
-
     public UrlResponse uploadFile(@RequestParam(value = "file") MultipartFile[] files) {
 
         // Create a response object to store the URLs of the uploaded files
-
         UrlResponse urlResponse = new UrlResponse();
         List<String> urls = new ArrayList<>();
         try {
-
             // Iterate through the uploaded files
-
             for (MultipartFile file : files) {
-
                 // Upload the file to Amazon S3 and get the URL
-
                 String url = storageServiceInterface.uploadFile(file);
-
                 // Add the URL to the list
-
                 urls.add(url);
             }
-
             // Set the list of URLs in the response object
-
             urlResponse.setUrls(urls);
         } catch (Exception e) {
-
-            // Handle any exceptions that may occur, such as network errors
-
             log.error("Network Error in fetching Amazon S3");
         }
-
         // Return the response object containing the uploaded file URLs
-
         return urlResponse;
     }
 
@@ -79,40 +60,30 @@ public class StorageController {
     @PostMapping("/uploadProfileImage")
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasAnyAuthority('OWNER', 'BORROWER')")
-
     public ResponseEntity<String> uploadProfileFile(@RequestParam("file") MultipartFile file) {
         try {
             // Upload the provided profile image file to Amazon S3
             String url = storageServiceInterface.uploadFile(file);
-
             // Create a JSON response containing the URL of the uploaded profile image
             JSONObject jsonResponse = new JSONObject();
             jsonResponse.put("url", url);
-
             // Return a ResponseEntity with the JSON response and HTTP status OK (200)
             return ResponseEntity.ok(jsonResponse.toString());
         } catch (Exception e) {
-            // Handle any exceptions that may occur during the upload process
-            // Return a ResponseEntity with HTTP status INTERNAL_SERVER_ERROR (500)
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-
-
 
     // API: To download the image of the product from Amazon S3
     @ApiOperation(value = "API : To download the image of the product from Amazon S3")
     @GetMapping("/download/{fileName}")
     @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("hasAuthority('OWNER')")
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
         // Retrieve the binary data (image) from Amazon S3 based on the provided file name
         byte[] data = storageServiceInterface.downloadFile(fileName);
-
         // Create a ByteArrayResource to represent the binary data
         ByteArrayResource resource = new ByteArrayResource(data);
-
         // Return a ResponseEntity with the binary data, content length, content type, and attachment header
         return ResponseEntity
                 .ok()
@@ -122,33 +93,28 @@ public class StorageController {
                 .body(resource);
     }
 
-
     // API: To view the uploaded image
     @ApiOperation(value = "API : To view the uploaded image")
     @GetMapping("/view/{fileName}")
     @ResponseStatus(HttpStatus.OK)
-
     // PreAuthorize annotation restricts access to authorized users with specified authorities (OWNER, BORROWER, ADMIN)
-    @PreAuthorize("hasAnyAuthority('OWNER', 'BORROWER', 'ADMIN')")
-
-    public void viewFile(@PathVariable String fileName, HttpServletRequest request, HttpServletResponse response) {
-
+    public void viewFile(
+            @PathVariable String fileName,
+            HttpServletResponse response) {
         // Delegate the task of viewing the image to the storageServiceInterface
         storageServiceInterface.viewFile(fileName, response);
     }
-
-
 
     // API: To delete the image in S3 cloud storage
     @ApiOperation(value = "API : To delete the image in S3 cloud storage")
     @DeleteMapping("/delete/{fileName}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     // This method deletes the specified image file in S3 storage
     public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
-
         // Delegate the task of deleting the file to the storageServiceInterface and return the result
-        return new ResponseEntity<>(storageServiceInterface.deleteFile(fileName), HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(
+                storageServiceInterface.deleteFile(fileName),
+                HttpStatus.NO_CONTENT);
     }
-
 }
--- src/test/java/com/nineleaps/leaps/controller/StorageControllerTest.java
+++ src/test/java/com/nineleaps/leaps/controller/StorageControllerTest.java
@@ -4,7 +4,10 @@ import com.nineleaps.leaps.dto.UrlResponse;
 import com.nineleaps.leaps.service.StorageServiceInterface;
 import com.nineleaps.leaps.utils.Helper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.core.io.ByteArrayResource;
@@ -13,30 +16,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@Tag("unit")
 class StorageControllerTest {
 
     @Mock
     private StorageServiceInterface storageServiceInterface;
-
+    @InjectMocks
     private StorageController storageController;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        storageController = new StorageController(storageServiceInterface);
     }
 
     @Test
+    @DisplayName("Upload File")
     void uploadFile() {
         // Prepare
-        MockMultipartFile file1 = new MockMultipartFile("file", "filename1.txt", "text/plain", "File content 1".getBytes());
-        MockMultipartFile file2 = new MockMultipartFile("file", "filename2.txt", "text/plain", "File content 2".getBytes());
+        MockMultipartFile file1 = new MockMultipartFile(
+                "file",
+                "filename1.txt",
+                "text/plain", "File content 1".getBytes());
+
+        MockMultipartFile file2 = new MockMultipartFile(
+                "file",
+                "filename2.txt",
+                "text/plain", "File content 2".getBytes());
+
         MultipartFile[] files = {file1, file2};
 
         when(storageServiceInterface.uploadFile(any(MultipartFile.class))).thenReturn("example-url");
@@ -50,6 +61,7 @@ class StorageControllerTest {
     }
 
     @Test
+    @DisplayName("Upload File - Exception")
     void uploadFile_withException() {
         // Prepare
         MockMultipartFile file1 = new MockMultipartFile("file", "filename1.txt", "text/plain", "File content 1".getBytes());
@@ -66,6 +78,7 @@ class StorageControllerTest {
     }
 
     @Test
+    @DisplayName("Upload Profile Image")
     void uploadProfileFile() {
         // Prepare
         MultipartFile file = new MockMultipartFile("file", "filename1.txt", "text/plain", "File content 1".getBytes());
@@ -80,6 +93,7 @@ class StorageControllerTest {
     }
 
     @Test
+    @DisplayName("Upload Profile Image - Exception")
     void uploadProfileFile_withException() {
         // Prepare
         MultipartFile file = new MockMultipartFile("file", "filename1.txt", "text/plain", "File content 1".getBytes());        when(storageServiceInterface.uploadFile(any(MultipartFile.class))).thenThrow(RuntimeException.class);
@@ -93,6 +107,7 @@ class StorageControllerTest {
     }
 
     @Test
+    @DisplayName("Download File")
     void downloadFile() {
         // Prepare
         String fileName = "example-file";
@@ -111,20 +126,21 @@ class StorageControllerTest {
 
     //
     @Test
+    @DisplayName("View File")
     void viewFile() {
         // Prepare
         String fileName = "example-file";
         HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpServletRequest request = mock(HttpServletRequest.class);
 
         // Execute
-        storageController.viewFile(fileName, request,response);
+        storageController.viewFile(fileName, response);
 
         // Verify
         verify(storageServiceInterface, times(1)).viewFile(fileName, response);
     }
 
     @Test
+    @DisplayName("Delete File")
     void deleteFile() {
         // Prepare
         String fileName = "example-file";

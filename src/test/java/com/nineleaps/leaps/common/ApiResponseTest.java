47738878ed304a18cedbe45eef0114f373bfb777
Mayank <thatmayank@gmail.com>
2023-10-20 11:33:31 +0530

    Refactor: @Tag and @DisplayName to ApiResponseTest

--- src/test/java/com/nineleaps/leaps/common/ApiResponseTest.java
+++ src/test/java/com/nineleaps/leaps/common/ApiResponseTest.java
@@ -1,13 +1,19 @@
 package com.nineleaps.leaps.common;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Tag("unit")
+@DisplayName("Api Response Test")
 class ApiResponseTest {
 
+    @InjectMocks
     private ApiResponse apiResponse;
 
     @BeforeEach
@@ -16,16 +22,19 @@ class ApiResponseTest {
     }
 
     @Test
+    @DisplayName("Get Timestamp Test")
     void getTimestamp() {
         assertNotNull(apiResponse.getTimestamp());
     }
 
     @Test
+    @DisplayName("Is Success Test")
     void isSuccess() {
         assertTrue(apiResponse.isSuccess());
     }
 
     @Test
+    @DisplayName("Get Message Test")
     void getMessage() {
         assertEquals("Success", apiResponse.getMessage());
     }

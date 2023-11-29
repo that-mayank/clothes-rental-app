61a31a3fac9519450e126c10f3c035e52e1631b7
yokesnineleaps <yokes.e@nineleaps.com>
2023-11-29 12:15:37 +0530

    Updated Test Cases. Overall Code Coverage 93.7%.

--- src/test/java/com/nineleaps/leaps/model/categories/CategoryTest.java
+++ src/test/java/com/nineleaps/leaps/model/categories/CategoryTest.java
@@ -1,6 +1,7 @@
 package com.nineleaps.leaps.model.categories;
 
 import com.nineleaps.leaps.dto.category.CategoryDto;
+import com.nineleaps.leaps.model.User;
 import com.nineleaps.leaps.model.product.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -11,6 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @Tag("unit_tests")
 @DisplayName("Category Tests")
 class CategoryTest {
@@ -61,6 +65,45 @@ class CategoryTest {
         assertEquals(categoryDto.getCategoryName(), category.getCategoryName());
         assertEquals(categoryDto.getDescription(), category.getDescription());
         assertEquals(categoryDto.getImageUrl(), category.getImageUrl());
+    }
+
+    @Test
+     void testSetAuditColumnsCreate() {
+        // Create a User object with mock data
+        User user = mock(User.class);
+        LocalDateTime createdAt = LocalDateTime.now().minusDays(1); // Example createdAt date
+        long createdBy = 123L; // Example createdBy value
+
+        // Mocking behavior of user object
+        when(user.getCreatedAt()).thenReturn(createdAt);
+        when(user.getCreatedBy()).thenReturn(createdBy);
+
+        // Create a Category instance
+        Category category = new Category();
+
+        // Call the setAuditColumnsCreate method with the mocked user object
+        category.setAuditColumnsCreate(user);
+
+        // Validate the Category object's fields are set correctly
+        assertEquals(createdAt, category.getCategoryCreatedAt());
+        assertEquals(Long.valueOf(createdBy), category.getCategoryCreatedBy());
+    }
+
+    @Test
+     void testSetAuditColumnsUpdate() {
+        // Example user ID
+        Long userId = 456L;
+
+        // Create a Category instance
+        Category category = new Category();
+
+        // Call the setAuditColumnsUpdate method with the example user ID
+        category.setAuditColumnsUpdate(userId);
+
+        // Validate the Category object's fields are set correctly
+        assertEquals(userId, category.getCategoryUpdatedBy());
+        // Ensure categoryUpdatedAt is not null (may differ slightly from current time due to execution time)
+        assertEquals(true, category.getCategoryUpdatedAt() != null);
     }
 }
 
--- src/test/java/com/nineleaps/leaps/model/categories/SubCategoryTest.java
+++ src/test/java/com/nineleaps/leaps/model/categories/SubCategoryTest.java
@@ -2,6 +2,7 @@ package com.nineleaps.leaps.model.categories;
 
 
 import com.nineleaps.leaps.dto.category.SubCategoryDto;
+import com.nineleaps.leaps.model.User;
 import com.nineleaps.leaps.model.product.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -11,6 +12,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @Tag("unit_tests")
 @DisplayName("SubCategory Tests")
 class SubCategoryTest {
@@ -60,6 +64,84 @@ class SubCategoryTest {
         assertEquals(subCategoryDto.getSubcategoryName(), subCategory.getSubcategoryName());
         assertEquals(subCategoryDto.getImageURL(), subCategory.getImageUrl());
         assertEquals(subCategoryDto.getDescription(), subCategory.getDescription());
+    }
+
+//    @Test
+//     void testSetAuditColumnsCreate() {
+//        // Create a User object with mock data
+//        User user = mock(User.class);
+//        LocalDateTime createdAt = LocalDateTime.now().minusDays(1); // Example createdAt date
+//        long createdBy = 123L; // Example createdBy value
+//
+//        // Mocking behavior of user object
+//        when(user.getCreatedAt()).thenReturn(createdAt);
+//        when(user.getCreatedBy()).thenReturn(createdBy);
+//
+//        // Create a Category instance
+//        Category category = new Category();
+//
+//        // Call the setAuditColumnsCreate method with the mocked user object
+//        category.setAuditColumnsCreate(user);
+//
+//        // Validate the Category object's fields are set correctly
+//        assertEquals(createdAt, category.getCategoryCreatedAt());
+//        assertEquals(Long.valueOf(createdBy), category.getCategoryCreatedBy());
+//    }
+//
+//    @Test
+//     void testSetAuditColumnsUpdate() {
+//        // Example user ID
+//        Long userId = 456L;
+//
+//        // Create a Category instance
+//        Category category = new Category();
+//
+//        // Call the setAuditColumnsUpdate method with the example user ID
+//        category.setAuditColumnsUpdate(userId);
+//
+//        // Validate the Category object's fields are set correctly
+//        assertEquals(userId, category.getCategoryUpdatedBy());
+//        // Ensure categoryUpdatedAt is not null (may differ slightly from current time due to execution time)
+//        assertEquals(true, category.getCategoryUpdatedAt() != null);
+//    }
+
+    @Test
+     void testSetAuditColumnsCreate() {
+        // Create a User object with mock data
+        User user = mock(User.class);
+        LocalDateTime createdAt = LocalDateTime.now().minusDays(1); // Example createdAt date
+        long createdBy = 123L; // Example createdBy value
+
+        // Mocking behavior of user object
+        when(user.getCreatedAt()).thenReturn(createdAt);
+        when(user.getCreatedBy()).thenReturn(createdBy);
+
+        // Create a Category instance
+        Category category = new Category();
+
+        // Call the setAuditColumnsCreate method with the mocked user object
+        category.setAuditColumnsCreate(user);
+
+        // Validate the Category object's fields are set correctly
+        assertEquals(createdAt, category.getCategoryCreatedAt());
+        assertEquals(Long.valueOf(createdBy), category.getCategoryCreatedBy());
+    }
+
+    @Test
+     void testSetAuditColumnsUpdate() {
+        // Example user ID
+        Long userId = 456L;
+
+        // Create a Category instance
+        Category category = new Category();
+
+        // Call the setAuditColumnsUpdate method with the example user ID
+        category.setAuditColumnsUpdate(userId);
+
+        // Validate the Category object's fields are set correctly
+        assertEquals(userId, category.getCategoryUpdatedBy());
+        // Ensure categoryUpdatedAt is not null (may differ slightly from current time due to execution time)
+        assertEquals(true, category.getCategoryUpdatedAt() != null);
     }
 }
 
